6e3f9f9983919ca83cc97a94ccab766c9af9dde7
Jendorski <gabjide@gmail.com>
2017-01-12 14:08:37 +0100

    Create event-create.ts

---
+++ src/pages/event-create/event-create.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { EventData } from '../../providers/event-data';
+
+@Component({
+  selector: 'page-event-create',
+  templateUrl: 'event-create.html',
+})
+export class EventCreatePage {
+
+  constructor(public nav: NavController, public eventData: EventData) {
+    this.nav = nav;
+    this.eventData = eventData;
+  }
+
+createEvent(eventName: string, eventDate: string, 
+  eventPrice: number, eventCost: number) {
+  this.eventData.createEvent(eventName, eventDate, eventPrice, eventCost).then( () => {
+    this.nav.pop();
+  });
+}
+
+}
